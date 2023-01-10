c3ce15319af313ea0aa8c5c9a41bd8f80ca8ffef
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-10 11:16:28 -0600

    Calculator basic layout complete

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,61 +1,40 @@
+import React from 'react';
+
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <article className="calculator-grid">
-      <div className="output">
-        <div className="previous-operand">1234</div>
-        <div className="current-operand">5678</div>
-      </div>
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-      <button type="button">÷</button>
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button">x</button>
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button">-</button>
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button">+</button>
-      <button type="button" className="span-two">0</button>
-      <button type="button">.</button>
-      <button type="button">=</button>
-    </article>
+        <div className="output">
+          <div className="previous-operand">1234 +</div>
+          <div className="current-operand">5678</div>
+        </div>
+        <button type="button" className="btn">AC</button>
+        <button type="button" className="btn">+/-</button>
+        <button type="button" className="btn">%</button>
+        <button type="button" className="btn btn--orange">÷</button>
+        <button type="button" className="btn">7</button>
+        <button type="button" className="btn">8</button>
+        <button type="button" className="btn">9</button>
+        <button type="button" className="btn btn--orange">x</button>
+        <button type="button" className="btn">4</button>
+        <button type="button" className="btn">5</button>
+        <button type="button" className="btn">6</button>
+        <button type="button" className="btn btn--orange">-</button>
+        <button type="button" className="btn">1</button>
+        <button type="button" className="btn">2</button>
+        <button type="button" className="btn">3</button>
+        <button type="button" className="btn btn--orange">+</button>
+        <button type="button" className="btn span-two">0</button>
+        <button type="button" className="btn">.</button>
+        <button type="button" className="btn btn--orange">=</button>
+      </article>
     );
   }
 }
 
-// const Calculator = () => (
-//   <article className="calculator-grid">
-//     <div className="output">
-//       <div className="previous-operand">1234</div>
-//       <div className="current-operand">5678</div>
-//     </div>
-//     <button type="button">AC</button>
-//     <button type="button">+/-</button>
-//     <button type="button">%</button>
-//     <button type="button">÷</button>
-//     <button type="button">7</button>
-//     <button type="button">8</button>
-//     <button type="button">9</button>
-//     <button type="button">x</button>
-//     <button type="button">4</button>
-//     <button type="button">5</button>
-//     <button type="button">6</button>
-//     <button type="button">-</button>
-//     <button type="button">1</button>
-//     <button type="button">2</button>
-//     <button type="button">3</button>
-//     <button type="button">+</button>
-//     <button type="button" className="span-two">0</button>
-//     <button type="button">.</button>
-//     <button type="button">=</button>
-//   </article>
-// );
-
-module.exports = Calculator;
+export default Calculator;
